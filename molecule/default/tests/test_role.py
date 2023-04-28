9f16a8e73394b2eb1a961b14964ce59bd07e84d5
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 12:43:49 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,20 +5,27 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('memcached'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("memcached"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('root', 'root', '/etc/memcached.conf'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("root", "root", "/etc/memcached.conf"),
+    ],
+)
 def test_memcached_config_file(host, username, groupname, path):
     memcached_config = host.file(path)
     assert memcached_config.exists
